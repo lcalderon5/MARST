d363d4351e900500677733d5cdf751e76420dc22
Lcalderon <L.CalderondelRio@student.tudelft.nl>
2025-01-17 15:51:09 +0100

    Added function to convert from orbital elements to cartesian

--- Config/spacecraft.py
+++ Config/spacecraft.py
@@ -3,9 +3,34 @@
 
 import numpy as np
 from collections import namedtuple
+from bodies_data import Earth
 
 # Using named tuples to make it more numba friendly, they are immutable and faster than dictionaries
 
+# Initial conditions for the spacecraft
+# --------- INPUTS ------------
+
+# Initial Orbit
+Periapsis = 100 # h in km
+Apoapsis = 5000 # h in km
+Inclination = 10    # Inclination in degrees
+Rigth_Ascension_node = 30 # Right Ascension of the Ascending Node in degrees
+Argument_periapsis = 40 # deg
+Initial_anomaly = 180 # deg
+
+# Calculate semi-major axis and eccentricity
+Apoapsis = Apoapsis + Earth.radius_equator
+Periapsis = Periapsis + Earth.radius_equator
+a = (Apoapsis + Periapsis) / 2 # in km
+e = (Apoapsis - Periapsis) / (Apoapsis + Periapsis) # unitless
+
+# Convert these to a position and velocity vector
+initial_position = np.array([0, 0, 0])  # Initial position in km
+initial_velocity = np.array([0, 0, 0])  # Initial velocity in km/s
+
+
+
+# Define named tuple for the spacecraft data
 # Feel free to add more parameters to the spacecraft, such as fuel consumption, thrust, etc.
 # Just make sure to name them in the namedtuple part and add them to the Spacecraft part as a value.
 
--- Modules/helper.py
+++ Modules/helper.py
@@ -8,9 +8,10 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import numpy as np
+from numba import njit
 from Config import bodies_data as bd
 
-
+@njit
 def sc_heigth(pos):
     """
     This function calculates the height of the spacecraft, taking into account the earth's change in radius over its latitude.
@@ -28,3 +29,62 @@ def sc_heigth(pos):
     return height
 
 
+# Function to set intial conditions
+@njit
+def orbital_elements_to_cartesian(mu, peri, apo, i, raan, arg_periapsis, init_anomaly):
+
+    # Convert some stuff to radians
+    i = np.radians(i)
+    raan = np.radians(raan)
+    arg_periapsis = np.radians(arg_periapsis)
+    init_anomaly = np.radians(init_anomaly)
+
+    # Calculate semi-major axis and eccentricity
+    a = (peri + apo) / 2  # km
+    e = (apo - peri) / (apo + peri)  # unitless
+
+    # Calculate position and velocity in the perifocal frame
+    p = a * (1 - e**2)  # Semi-latus rectum, km
+    r = p / (1 + e * np.cos(init_anomaly))  # Radius, km
+
+    # Position vector in perifocal coordinates
+    r_peri = np.array([
+        r * np.cos(init_anomaly),
+        r * np.sin(init_anomaly),
+        0
+    ])
+
+    # Velocity vector in perifocal coordinates
+    v_peri = np.array([
+        -np.sqrt(mu / p) * np.sin(init_anomaly),
+        np.sqrt(mu / p) * (e + np.cos(init_anomaly)),
+        0
+    ])
+
+    # Rotation matrices
+    R3_raan = np.array([
+        [np.cos(-raan), np.sin(-raan), 0],
+        [-np.sin(-raan), np.cos(-raan), 0],
+        [0, 0, 1]
+    ])
+
+    R1_incl = np.array([
+        [1, 0, 0],
+        [0, np.cos(-i), np.sin(-i)],
+        [0, -np.sin(-i), np.cos(-i)]
+    ])
+
+    R3_arg_peri = np.array([
+        [np.cos(-arg_periapsis), np.sin(-arg_periapsis), 0],
+        [-np.sin(-arg_periapsis), np.cos(-arg_periapsis), 0],
+        [0, 0, 1]
+    ])
+
+    # Correct combined rotation matrix
+    R_total = R3_arg_peri @ R1_incl @ R3_raan
+
+    # Transform to inertial frame
+    r_inertial = R_total @ r_peri
+    v_inertial = R_total @ v_peri
+
+    return r_inertial, v_inertial
